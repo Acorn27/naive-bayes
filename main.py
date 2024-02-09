5590e93fb8e282d6f8d53ed215af729a912e57b2
Huy <93058128+Acorn27@users.noreply.github.com>
2024-02-09 17:54:35 +0000

    add input argument

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-"""Call Naives Bayes"""
+"""Naives Bayes Model"""
+import sys
 from util import load_training_data
 from naive_bayes import NaiveBayes
 
 
-def main():
+def main(input_file_name):
     """ Main function"""
-    sentences, labels = load_training_data('dataset1.txt')
+    sentences, labels = load_training_data(input_file_name)
 
     my_model = NaiveBayes(use_smoothing=True)
 
     my_model.fit(training_sentences=sentences, labels_list=labels)
 
-    c = my_model.predict("Chinese Chinese Chinese Tokyo Japan")
+    c = my_model.predict("gently embraces blue ripples", detail=True)
 
-    print(c)
+    print(f"Predicted class is {c}")
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python <program_name.py> <input_file.txt>")
+    input_file_name = sys.argv[1]
+    main(input_file_name)
