e8e40d2f67334f90790bd3505115f361ae78f2fa
Huy <93058128+Acorn27@users.noreply.github.com>
2024-02-09 17:54:58 +0000

    unknown word exception

---
+++ exceptions/exceptions.py
@@ -0,0 +1,14 @@
+class UnknownWord(ValueError):
+    """ Raise inplace value error when words that
+    occur in our test data but are not in our vocabulary at al"""
+    def __init__(self, feature):
+        """
+        Constructor for UnknowWord exception
+        :param feature: word that founded to be "unknown"
+        """
+        message = self.message(feature)
+        super(UnknownWord, self).__init__(message)
+
+    @staticmethod
+    def message(feature):
+        return ("Unknown word caught: " + str(feature) + " never occur in the training data. Skip")
